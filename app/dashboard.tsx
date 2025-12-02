79d34e3c8886963daaf7fbd28ef96d74314e08a7
EstefanoCampana <Estefano.CampanaMunoz@edu.sait.ca>
2025-12-02 16:12:49 -0700

    Add and retrieve employees functionality

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,10 +1,12 @@
 import { logout } from "@/utils/logout";
 import { Redirect, router } from "expo-router";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { Formik } from "formik";
-import React from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import * as Yup from "yup";
 import { useAuth } from "../hooks/useAuth";
+import { db } from "../lib/firebase";
 
 const EmployeeSchema = Yup.object().shape({
   name: Yup.string().required("Required"),
@@ -14,8 +16,43 @@ const EmployeeSchema = Yup.object().shape({
   phone: Yup.string().required("Required"),
 });
 
+interface FormValues {
+  name: string;
+  department: string;
+  position: string;
+  employeeID: string;
+  phone: string;
+}
+interface Employee extends FormValues {
+  id: string;
+}
+
 const Dashboard = () => {
   const { user, loading } = useAuth();
+  const initialValues = {name: "",
+            department: "",
+            position: "",
+            employeeID: "",
+            phone: "",}
+  const [employees, setEmployees] = useState<Employee[]>([]);
+ 
+useEffect(() => {
+  async function loadProfile() {
+    const employeesRef = collection(db, "employees");
+    const snap = await getDocs(employeesRef);
+ 
+    if (!snap.empty) {
+      const list: Employee[] = snap.docs.map((docSnap) => ({
+        id: docSnap.id,
+        ...(docSnap.data() as FormValues),
+      }));
+ 
+      setEmployees(list);
+    }
+  }
+ 
+  loadProfile();
+}, []);
 
   // Show loading spinner while checking authentication
   if (loading) {
@@ -30,6 +67,23 @@ const Dashboard = () => {
   if (!user) {
     return <Redirect href="/" />;
   }
+  
+  const submitForm = async (values: FormValues, actions: any) => {
+    try{
+          await addDoc(collection(db, "employees"),{
+          name: values.name,
+          department: values.department,
+          position: values.position,
+          employeeID: values.employeeID,
+          phone: values.phone
+        });
+        actions.resetForm()
+      } catch (firestoreError) {
+        console.log(
+          firestoreError
+        );
+      }
+  }
 
   const handleLogOut = async () => {
     await logout();
@@ -38,25 +92,21 @@ const Dashboard = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome! {user.displayName}</Text>
-      <Text style={styles.userEmail}>{user.email}</Text>
+      <View style={{flex:1, flexDirection: "column", alignSelf: "flex-start"}}>
+        <Text style={styles.welcome}>Welcome! {user.displayName}</Text>
+        <Text style={styles.userEmail}>{user.email}</Text>
         <Text style={styles.title}>Employee Information</Text>
 
         <Formik
-          initialValues={{
-            name: "",
-            department: "",
-            position: "",
-            employeeID: "",
-            phone: "",
-          }}
+          initialValues={initialValues}
           validationSchema={EmployeeSchema}
-          onSubmit={(values) => console.log(values)}
+          onSubmit={submitForm}
+          
         >
           {({
             handleChange,
+            handleSubmit,
             handleBlur,
-            submitForm,
             values,
             errors,
             touched,
@@ -113,17 +163,33 @@ const Dashboard = () => {
               />
               {touched.phone && errors.phone && <Text style={styles.error}>{errors.phone}</Text>}
 
+              <View style={{flexDirection: "row", alignItems: "center", gap: 20, justifyContent: "center"}}>
               <TouchableOpacity
-              onPress={submitForm}  
+              onPress={() => handleSubmit()}  
               style={styles.submitButton}>
                 <Text style={styles.submitButtonText}>Submit</Text>
               </TouchableOpacity>
+
+            <TouchableOpacity style={styles.logOutButton} onPress={handleLogOut}>
+            <Text style={styles.logoutText}>Log Out</Text>
+          </TouchableOpacity>
+          </View>
             </>
           )}
         </Formik>
-      <TouchableOpacity style={styles.logOutButton} onPress={handleLogOut}>
-        <Text style={styles.logoutText}>Log Out</Text>
-      </TouchableOpacity>
+      </View>
+
+      <ScrollView style={{flex:1, alignContent: "center", borderRadius: 10, backgroundColor:"#fff", paddingTop: 75, borderWidth: 2}}>
+        <Text style={styles.title}>Employees</Text> {employees.map((emp) => (
+          <View key={emp.id} style={{ margin: 5, borderRadius: 10, backgroundColor:"#2563eb",borderWidth: 2, borderColor: "#fff", padding: 30}}>
+            <Text style={{fontSize: 16, textAlign: "center", color: "#fff"}}>Name: {emp.name}</Text>
+            <Text style={{fontSize: 16, textAlign: "center", color: "#fff"}}>Department: {emp.department}</Text>
+            <Text style={{fontSize: 16, textAlign: "center", color: "#fff"}}>Position: {emp.position}</Text>
+            <Text style={{fontSize: 16, textAlign: "center", color: "#fff"}}>Employee ID: {emp.employeeID}</Text>
+            <Text style={{fontSize: 16, textAlign: "center", color: "#fff"}}>Phone: {emp.phone}</Text>
+          </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
@@ -134,20 +200,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f0f4f8",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+    gap: 20,
     alignItems: "center",
     padding: 20,
+    flexDirection: "row"
   },
   welcome: {
     fontSize: 26,
     fontWeight: "700",
     marginBottom: 10,
     color: "#1f2938",
+    textAlign: "center"
   },
   userEmail: {
     fontSize: 18,
     color: "#4b5563",
     marginBottom: 30,
+    textAlign: "center"
   },
   logoutText: {
     color: "#ffffff",
@@ -156,18 +226,16 @@ const styles = StyleSheet.create({
   },
   logOutButton: {
     backgroundColor: "#ef4444",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    marginTop: 20
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    borderRadius: 10,
   },
   formsButton: {
     backgroundColor: "#3c00ffff",
     color: "#ffffff",
-    margin: 10,
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    borderRadius: 10,
   },
   title: {
     fontSize: 26,
